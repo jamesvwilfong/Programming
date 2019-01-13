066ec9f70ef7e181b56dac98bf2c87912a394a8e
jamesvwilfong <46636932+jamesvwilfong@users.noreply.github.com>
2019-01-12 21:37:47 -0500

    Create House.java

---
+++ Java/Program2/House.java
@@ -0,0 +1,19 @@
+// Name: James Wilfong
+// Instructor: Viji
+// CSE 174, Section D
+// Date: 9/8/2016
+// FIlename: House.java
+// Description: Write a program that prints a house
+
+public class House {
+   
+   public static void main(String[] args) {
+      System.out.println("   +   ");
+      System.out.println("  + +  ");
+      System.out.println(" +   + ");
+      System.out.println("+-----+");
+      System.out.println("| .-. |");
+      System.out.println("| | | |");
+      System.out.println("+-+-+-+");
+   }
+}
