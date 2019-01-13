72c36624e2667d72cff790ed8939271630e599ae
jamesvwilfong <46636932+jamesvwilfong@users.noreply.github.com>
2019-01-12 21:40:25 -0500

    Create Letters.java

---
+++ Java/Program3/Letters.java
@@ -0,0 +1,36 @@
+// Name: James Wilfong
+// Instructor: Viji
+// CSE 174, Section D
+// Date: 9/16/2016
+// FIlename: Letters.java
+
+public class Letters {
+   
+   public static void main(String[] args) {
+  
+      final String LETTER_M = "    *     *\n   * *   * *"
+               + "\n  *   * *   *\n *     *     *\n*             *";
+      
+      final String LETTER_A = "       *      \n      * *"
+               + "\n     *   *\n    *     *\n   *********"
+               + "\n  *         *\n *           *\n*             *";
+      
+      final String LETTER_I = "***************\n       *"
+               + "\n       *\n       *\n       *\n       *\n"
+               + "       *\n***************";
+      
+      final String LETTER_L = "*              \n*              \n"
+               + "*              \n*              \n*              \n"
+               + "*              \n*              \n***************";
+      
+      
+      // Dislays the word MAIL
+      System.out.println(LETTER_M);
+      System.out.println();
+      System.out.println(LETTER_A);
+      System.out.println();
+      System.out.println(LETTER_I);
+      System.out.println();
+      System.out.println(LETTER_L);
+   }
+}
