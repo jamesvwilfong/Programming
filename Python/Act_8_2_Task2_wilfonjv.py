9a1da6536205ba5a2a8db0184cf5f3f1f74cc426
jamesvwilfong <46636932+jamesvwilfong@users.noreply.github.com>
2019-01-12 22:01:37 -0500

    Create Act_8_2_Task2_wilfonjv.py

---
+++ Python/Act_8_2_Task2_wilfonjv.py
@@ -0,0 +1,33 @@
+# Activity 8.2.1: Task 2
+# File: ACT8_2_1_Task2_wilfonjv.py
+# Date:    17 Oct 2018
+# By:      James Wilfong
+#          wilfonjv       
+# Section: 007
+# Team:    071
+#
+# ELECTRONIC SIGNATURE
+# James Wilfong
+#
+# The electronic signature above indicates the script
+# submitted for evaluation is my individual work, and I
+# have a general understanding of all aspects of its
+# development and execution.
+#
+# A BRIEF DESCRIPTION OF WHAT THE SCRIPT OR FUNCTION DOES
+# This program makes various calculations
+
+A = 10
+B = A / 2 * 20
+C = (3*A) / 2
+D = B + 3 * (A / 2) - C
+E = 13 / 3 * A
+F = (14 - E) * C
+G = 15 / 3
+H = 13 / 3 + 1 / 3
+I = 13 / 3 + 2 / 3
+J = 13 // 3 + 4 /3
+K = 4 ** (3*2)
+L = -4**1/2
+M = 4 ** (1/2)
+Z = 133 % 20
