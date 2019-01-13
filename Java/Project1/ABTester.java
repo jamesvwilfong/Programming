d9a32866938e6f3ac0212fd2a9f391078f5c8778
jamesvwilfong <46636932+jamesvwilfong@users.noreply.github.com>
2019-01-12 21:24:36 -0500

    Create ABTester.java

---
+++ Java/Project1/ABTester.java
@@ -0,0 +1,11 @@
+// James Wilfong
+// Dr. Stephan
+// CSE 271, Section C
+public class ABTester {
+	public static void main(String[] args) {
+        System.out.println("39 + 3");
+        System.out.println(39 + 3);
+        System.out.println("39" + 3);
+    }
+
+}//end Tester class
