c884e48952a6140f4c864934e2d875d85fb45930
jamesvwilfong <46636932+jamesvwilfong@users.noreply.github.com>
2019-01-12 19:16:23 -0500

    Create Lab4B.cpp

---
+++ lab4/Lab4B.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+//returns a true if an integer number is a prime or false if it is not
+bool isPrime(int a) {
+	bool result = true;
+	if (a <= 1)
+		result = false;
+	else if (a == 2)
+		result = true;
+	else {
+		for (int i = 2; i <= a / 2; i++) {
+			if (a%i == 0)
+				result = false;
+		}
+	}
+	return result;
+}
+int main() {
+
+	cout << isPrime(-5) << " Expected: 0" << endl;
+	cout << isPrime(11) << " Expected: 1" << endl;
+	cout << isPrime(8) << " Expected: 0" << endl;	
+	cout << isPrime(1) << " Expected: 0" << endl;
+	cout << isPrime(191) << " Expected: 1" << endl;
+}
