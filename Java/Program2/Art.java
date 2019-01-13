f7eecb6cf441be5af9d29fe9daad20b9eb1df84c
jamesvwilfong <46636932+jamesvwilfong@users.noreply.github.com>
2019-01-12 21:37:15 -0500

    Create Art.java

---
+++ Java/Program2/Art.java
@@ -0,0 +1,31 @@
+// Name: James Wilfong
+// Instructor: Viji
+// CSE 174, Section D
+// Date: 9/8/2016
+// FIlename: Art.java
+// Description: Write a program that prints an imitation 
+//              of a Piet Mondrian painting
+
+public class Art {
+   
+   public static void main(String[] args) {
+      
+      System.out.println("---------------");
+      System.out.println("|@@@|#########|");
+      System.out.println("|@@@|#########|");
+      System.out.println("|---|#########|");
+      System.out.println("|---|#########|");
+      System.out.println("|@@@|#########|");
+      System.out.println("|@@@|#########|");
+      System.out.println("|@@@|#########|");
+      System.out.println("|@@@|#########|");
+      System.out.println("|@@@|#########|");
+      System.out.println("|@@@|#########|");
+      System.out.println("|---|------|--|");
+      System.out.println("|&&&|@@@@@@|@@|");
+      System.out.println("|&&&|@@@@@@|--|");
+      System.out.println("|&&&|@@@@@@|%%|");
+      System.out.println("|&&&|@@@@@@|%%|");
+      System.out.println("---------------");
+   }
+}
