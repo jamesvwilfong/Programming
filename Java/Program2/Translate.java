cf87c2ab2ebe4a6b166827f2d344d9a4b1076e6b
jamesvwilfong <46636932+jamesvwilfong@users.noreply.github.com>
2019-01-12 21:38:16 -0500

    Create Translate.java

---
+++ Java/Program2/Translate.java
@@ -0,0 +1,30 @@
+// Name: James Wilfong
+// Instructor: Viji
+// CSE 174, Section D
+// Date: 9/11/2016
+// FIlename: Translate.java
+// Description: write a program that translates a list of phrases from english to spanish
+
+public class Translate {
+   
+   public static void main(String[] args) {
+      
+      System.out.print("English\t\t\t");
+      System.out.println("Spanish");
+      
+      System.out.print("-------------------------");
+      System.out.println("-----------------------");
+      
+      System.out.print("Good Morning\t\t");
+      System.out.println("Buenos Días");
+      
+      System.out.print("It is a pleasure to meet you\t");
+      System.out.println("Es un placer a concerte");
+      
+      System.out.print("Please call me tomorrow\t");
+      System.out.println("Llamame mañana por favor");
+     
+      System.out.print("Have a nice day\t\t");
+      System.out.println("Tengas un buen día");
+   }
+}
