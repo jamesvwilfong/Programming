14c0e7845d5b9f466b35b76f229bd7fbae51c256
jamesvwilfong <46636932+jamesvwilfong@users.noreply.github.com>
2019-01-12 21:51:26 -0500

    Create testexam.java

---
+++ Java/testexam.java
@@ -0,0 +1,13 @@
+public class testexam {
+   public static void main(String[] args) {
+      int x;
+      int y = 20;
+      double z = 30;
+      z = z +10;
+      x = y < 30 ? y + 4 : y + 5;
+      double gpa = 3.6234;
+      
+      System.out.println("\"this is \\a\\ \nice gpa:\"");
+      System.out.println(x);
+   }
+}
